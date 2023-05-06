48a64f961168752f36a4e6f861aea755cf5e6520
Zhan Rongrui <2742392377@qq.com>
2023-05-07 01:13:10 +0800

    fix bug in zrr-tools

--- dockers/zrr-tools/get_ip.py
+++ dockers/zrr-tools/get_ip.py
@@ -1,11 +1,33 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 app = FastAPI()
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
 
 @app.get("/")
 async def read_root(request: Request):
-    client_host = request.client.host
-    return client_host
+    client_host = request.headers.get("X-Real-IP")
+    x_forwarded_for = request.headers.get("X-Forwarded-For")
+
+    if x_forwarded_for:
+        client_host = x_forwarded_for.split(",")[0]
+    else:
+        client_host = request.client.host
+
+    return request.client.host
+
+@app.get("/v1/get_ip")
+async def v2_get_ip(request: Request):
+    client_host = request.headers.get("X-Real-IP")
+    x_forwarded_for = request.headers.get("X-Forwarded-For")
+
+    if x_forwarded_for:
+        client_host = x_forwarded_for.split(",")[0]
+    else:
+        client_host = request.client.host
+
+    return {"client_host": client_host}
 
 if __name__ == "__main__":
-    app()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, forwarded_allow_ips='*')
