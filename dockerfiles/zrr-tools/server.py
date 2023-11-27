f0edd0e8142c47e9d8b1c7de3295a45889f853c0
Zhan Rongrui <2742392377@qq.com>
2023-11-27 20:11:53 +0800

    rm dockerfile user

--- dockerfiles/zrr-tools/server.py
+++ dockerfiles/zrr-tools/server.py
@@ -4,29 +4,29 @@ from fastapi.middleware.trustedhost import TrustedHostMiddleware
 app = FastAPI()
 app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
 
-@app.get("/")
-async def read_root(request: Request):
+@app.get("/get_ip/v2")
+async def get_ip_v2(request: Request):
     client_host = request.headers.get("X-Real-IP")
     x_forwarded_for = request.headers.get("X-Forwarded-For")
-
     if x_forwarded_for:
         client_host = x_forwarded_for.split(",")[0]
-    else:
-        client_host = request.client.host
-
-    return request.client.host
-
-@app.get("/v1/get_ip")
-async def get_ip(request: Request):
-    client_host = request.headers.get("X-Real-IP")
-    x_forwarded_for = request.headers.get("X-Forwarded-For")
-
-    if x_forwarded_for:
-        client_host = x_forwarded_for.split(",")[0]
-    else:
+    elif request.client is not None:
         client_host = request.client.host
 
     return {"client_host": client_host}
+
+
+@app.get("/get_ip")
+@app.get("/get_ip/v1")
+async def get_ip_v1(request: Request):
+    client_host = request.headers.get("X-Real-IP")
+    x_forwarded_for = request.headers.get("X-Forwarded-For")
+    if x_forwarded_for:
+        client_host = x_forwarded_for.split(",")[0]
+    elif request.client is not None:
+        client_host = request.client.host
+
+    return client_host
 
 if __name__ == "__main__":
     import uvicorn
